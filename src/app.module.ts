fa479a5be08d2fa9b8b6b657e9101cc53d43465e
Your Name <you@example.com>
2025-09-05 09:41:31 +0000

    install dependencies for TypeOrm and .env file and add modules to app.modules.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
+// app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, type TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (cfg: ConfigService): TypeOrmModuleOptions => ({
+        type: 'postgres',
+        host: cfg.get<string>('DB_HOST') ?? '127.0.0.1',
+        port: Number(cfg.get('DB_PORT') ?? 5432),
+        username: cfg.get<string>('DB_USER') ?? 'postgres',
+        password: cfg.get<string>('DB_PASS') ?? '',
+        database: cfg.get<string>('DB_NAME') ?? 'postgres',
+        autoLoadEntities: true,
+        synchronize: true, // тільки для dev
+        logging: cfg.get('NODE_ENV') === 'development',
+      }),
+    }),
+  ],
 })
 export class AppModule {}
