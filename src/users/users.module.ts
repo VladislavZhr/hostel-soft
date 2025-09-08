5de242ac6ae599f7f4b19830d20ea203c1444cab
VladyslavZhr <strelkov.vlad2017@gmail.com>
2025-09-08 18:30:44 +0000

    fixing bag in users.module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,11 +4,10 @@ import { User } from './entities/user.entity';
 import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { AuthController } from '../auth/auth.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersRepository, UsersService, UsersController],
+  providers: [UsersRepository, UsersService],
   controllers: [UsersController],
   exports: [UsersService],
 })
