f4ce0a871df801c8c8b443540ed3fea6986fdf18
VladyslavZhr <strelkov.vlad2017@gmail.com>
2025-09-09 17:59:50 +0000

    add inventory logic

---
+++ src/inventory/entities/InventoryKind.ts
@@ -0,0 +1,15 @@
+
+
+export enum InventoryKind {
+  TULLE = 'tulle',
+  CURTAINS = 'curtains',
+  BLANKET = 'blanket',
+  MATTRESS = 'mattress',
+  PILLOWCASE = 'pillowcase',
+  MATTRESS_COVER = 'mattressCover',
+  DUVET_COVER = 'duvetCover',
+  TOWEL = 'towel',
+  SHEET = 'sheet',
+  BEDSPREAD = 'cover',
+  PILLOW = 'pillow',
+}
